b0213578038f9e0a8607367773e1f916d249a873
EmmanuelMess <emmanuelbendavid@gmail.com>
2023-07-11 19:06:41 -0300

    Fix tests

--- code/test/tests.cpp
+++ code/test/tests.cpp
@@ -1,14 +1,44 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <lexy/action/parse.hpp>
+#include <lexy/callback.hpp>
+#include <lexy/dsl.hpp>
+#include <lexy/input/file.hpp>
+#include <lexy_ext/report_error.hpp>
+#include <lexy/input/string_input.hpp>
 
-#include <ConstraintBasedSimulator/sample_library.hpp>
+#include <Eigen/Core>
 
+struct Color {
+    std::uint8_t r, g, b;
+};
 
-TEST_CASE("Factorials are computed", "[factorial]")
-{
-  REQUIRE(factorial(0) == 1);
-  REQUIRE(factorial(1) == 1);
-  REQUIRE(factorial(2) == 2);
-  REQUIRE(factorial(3) == 6);
-  REQUIRE(factorial(10) == 3628800);
+namespace grammar {
+namespace dsl = lexy::dsl;
+
+struct channel {
+    static constexpr auto rule = dsl::integer<std::uint8_t>(dsl::n_digits<2, dsl::hex>);
+    static constexpr auto value = lexy::forward<std::uint8_t>;
+};
+
+struct color {
+    static constexpr auto rule = dsl::hash_sign + dsl::times<3>(dsl::p<channel>);
+    static constexpr auto value = lexy::construct<Color>;
+};
+} // namespace grammar
+
+TEST_CASE("lexy works", "[lexy base]") {
+    auto literal = lexy::zstring_input("#FF00FF");
+    auto result = lexy::parse<grammar::color>(literal, lexy_ext::report_error);
+
+    REQUIRE(result);
+}
+
+TEST_CASE("Eigen works", "[eigen base]") {
+    Eigen::Matrix3d matrix = Eigen::Matrix3d::Random();
+    matrix = (matrix + Eigen::Matrix3d::Constant(1.2)) * 50;
+
+    const Eigen::Vector3d vector(1,2,3);
+
+    REQUIRE((matrix * vector - Eigen::Vector3d(404.274,512.237,261.153)).sum() <= 0.02);
 }
