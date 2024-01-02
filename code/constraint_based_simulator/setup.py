4cc01582cb4044a42593eacf0a9041f781444b48
EmmanuelMess <emmanuelbendavid@gmail.com>
2024-01-02 16:33:48 -0300

    Add setup.py

---
+++ code/constraint_based_simulator/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='Constraint Based Simulator',
+   version='0.1.0',
+   description='', # TODO add description
+   author='EmmanuelMess',
+   packages=['constraint_based_simulator'],
+   install_requires=['numpy', 'lark', 'PySide6'],
+)
