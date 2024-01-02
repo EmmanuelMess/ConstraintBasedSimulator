8e1f72721ae1d73b27088b0ee002283c73eaf902
EmmanuelMess <emmanuelbendavid@gmail.com>
2024-01-01 21:14:23 -0300

    Remove C++ code

--- code/src/main_app/input_reader/SemanticsCheck.cpp
+++
@@ -1,7 +0,0 @@
-#include "main_app/input_reader/SemanticsCheck.hpp"
-
-namespace input_reader::internal::semantics {
-bool SemanticsCheck::checkSemantics(const ast::SimulationState& simulationState) {
-    return !simulationState.empty();
-}
-}
