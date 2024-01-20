2df5c9533fb1c2a5842c88cd4c4f435299342b20
EmmanuelMess <emmanuelbendavid@gmail.com>
2024-01-20 17:33:52 -0300

    Remove unused initialization signal

--- code/constraint_based_simulator/events_manager/InitializationSignals.py
+++ code/constraint_based_simulator/events_manager/InitializationSignals.py
@@ -2,7 +2,6 @@ from constraint_based_simulator.events_manager.Signal import Signal
 from constraint_based_simulator.input_reader.SimulationFile import SimulationFile
 
 appInitialization: Signal = Signal()
-readFileProperties: Signal = Signal()
 grapherParameters: Signal[int, int] = Signal[int, int]()
 simulationPropertiesAvailable: Signal[SimulationFile] = Signal[SimulationFile]()
 simulatorLoaded: Signal = Signal()
