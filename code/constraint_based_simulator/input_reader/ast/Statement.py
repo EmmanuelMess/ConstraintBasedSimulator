aa70b9ab5f9c7f58f01d99660c77d71349e7cbfd
EmmanuelMess <emmanuelbendavid@gmail.com>
2024-01-22 18:38:15 -0300

    Fix statement type

--- code/constraint_based_simulator/input_reader/ast/Statement.py
+++ code/constraint_based_simulator/input_reader/ast/Statement.py
@@ -1,7 +1,8 @@
 from typing import Union
 
+from constraint_based_simulator.input_reader.ast.Constraint import Constraint
 from constraint_based_simulator.input_reader.ast.Point import Point
 from constraint_based_simulator.input_reader.ast.StaticQualifier import StaticQualifier
 
 
-Statement = Union[Point, StaticQualifier]
+Statement = Union[Point, StaticQualifier, Constraint]
